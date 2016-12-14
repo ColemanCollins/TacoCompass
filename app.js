ece0ca88ca397977479ed19896b5b68659a94d73
Coleman Collins <coleman@colemancollins.com>
2016-12-13 22:12:58 -0600

    pass-thru app for the TACOCOMPASS

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var request = require('request');
+
+var app = express();
+
+app.get('/tacos', function(req, res) {
+  console.log('getting tacos');
+  var latitude = req.query.latitude
+  var longitude = req.query.longitude
+  request({
+    method: 'GET',
+    url: 'https://api.yelp.com/v3/businesses/search?term=taqueria&latitude=' + latitude + '&longitude=' + longitude + '&limit=1&open_now=true&sort_by=distance', 
+    }, function(error,response,body) {
+      if (!error && response.statusCode == 200) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        res.send(body);
+        console.log(body);
+      } else {
+        res.status(502).send('yelp is mad at you')
+        console.log('yelp is mad at you');
+      }
+    })
+    .auth(false, false, true, process.env.YELP_BEARER_TOKEN)
+});
+
+app.listen(process.env.PORT || 5000)
